64369ae24447194b282d55f45a3a3e6c32113803
Sateesh. Mandanapu <85310226+Smandanapu@users.noreply.github.com>
2025-04-26 12:22:51 -0500

    cowsay correction

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo apt get install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon " run for cover , i am a DRAGON....RAWR"  >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
