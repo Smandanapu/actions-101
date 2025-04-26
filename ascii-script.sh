cfb5a2de3bf9b45880ae86e3f3c324f2e9f2f0a8
Sateesh. Mandanapu <85310226+Smandanapu@users.noreply.github.com>
2025-04-26 12:26:34 -0500

    tiger to dragon

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f tiger " run for cover , i am a tiger....RAWR"  >> tiger.txt
-grep -i "tiger"  tiger.txt
-cat tiger.txt
+cowsay -f dragon " run for cover , i am a dragon....RAWR"  >> dragon.txt
+grep -i "dragon"  dragon.txt
+cat dragon.txt
 ls -ltra
